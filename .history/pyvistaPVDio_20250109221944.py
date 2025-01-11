72cc7380a1b73ea098005b6187f87aabf0e7f02a
StefanoFochesatto <54640332+StefanoFochesatto@users.noreply.github.com>
2025-01-11 01:40:21 -0900

    workingish app, separated example with dockercompose

--- .history/pyvistaPVDio_20250109221944.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from stpyvista import stpyvista
-from firedrake import *
-from viamr import VIAMR
-from viamr.utility import SphereObstacleProblem
-from viamr.utility import SpiralObstacleProblem
-import threading
-from time import sleep
